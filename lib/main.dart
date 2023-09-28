7e26f1ca49f3813171c0bc2c84e9cfc3b4fb603d
KaushikDas9 <dasbabai2017@gmail.com>
2023-09-28 13:01:30 +0530

    uploaded

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'Home_screen/Home.dart';
 import 'Loading_screen/Loading.dart';
 import 'Location_screen/Location.dart';
 
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
 
-void main() {
-  runApp(  MaterialApp(
-  debugShowCheckedModeBanner: false,
-  initialRoute: "/Loading",
-    routes: {
-          "/Home" :  (context) =>  const Home1(),
-          "/Loading":  (context) =>  const Loading1(),
-          "/Location" :  (context) => const Location(),
-    },
-  ),
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: "/Loading",
+      routes: {
+        "/Home": (context) => const Home1(),
+        "/Loading": (context) => const Loading1(),
+        "/Location": (context) => const Location(),
+      },
+    ),
   );
 }
-
-
-
-
